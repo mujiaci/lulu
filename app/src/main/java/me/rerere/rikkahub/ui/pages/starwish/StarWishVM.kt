244e9872d0089bc5d5c548e95d6a50d684f54801
jiacimu-droid <1187725424@qq.com>
2026-07-01 22:05:48 +0800

    Import flow first for theater generation

--- app/src/main/java/me/rerere/rikkahub/ui/pages/starwish/StarWishVM.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/starwish/StarWishVM.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.SharingStarted
